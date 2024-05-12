0cf9b7c6a029d49c03a0152937c0deb77e7a18be
Itsyaboijeff01 <169049736+Itsyaboijeff01@users.noreply.github.com>
2024-05-12 12:40:17 +0200

    genre sql
    Script to create and populate the table.

---
+++ genre.sql
@@ -0,0 +1,13 @@
+use hoop;
+CREATE TABLE genre(
+	genreID int NOT NULL AUTO_INCREMENT,
+    genreName varchar(255) NOT NULL ,
+    PRIMARY KEY (genreID),
+    UNIQUE (genreName)
+);
+INSERT INTO genre (genreName) VALUES ("Action");
+INSERT INTO genre (genreName) VALUES ("Adventure");
+INSERT INTO genre (genreName) VALUES ("Comedy");
+INSERT INTO genre (genreName) VALUES ("Romance");
+INSERT INTO genre (genreName) VALUES ("Documentry");
+SELECT * FROM genre;
