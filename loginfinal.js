ebcc9093c5dd7318d7da02656037db4f1c5a8b8a
RNkwana <169057295+RNkwana@users.noreply.github.com>
2024-05-27 16:12:48 +0200

    Add files via upload
    Preliminary login files.

---
+++ loginfinal.js
@@ -0,0 +1,43 @@
+document.getElementById('loginForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+  
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+  
+    const data = {
+      email: email,
+      password: password,
+    };
+    console.log('Form submitted'); // Debugging statement
+    console.log('Data to send:', data); // Debugging statement
+  
+    fetch('login.php', {//API ENDPOINT
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+    .then(response => {
+      console.log('Response status:', response.status); // Debugging statement
+      return response.json();
+    })
+    .then(data => {
+      console.log('Response data:', data); // Debugging statement
+      if (data.status === 'success') {
+        alert('Login successful!');
+        // Redirect to home page
+        window.location.href= 'index.php';
+      } else {
+        alert('Login failed: ' + data.data || "Unknown error");
+        //redirect back to login.
+        window.location.href = 'registerpage.php';
+      }
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+      alert('An error occurred. Please try again later.');
+    });
+  });
+  
